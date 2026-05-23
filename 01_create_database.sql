17e346834a9cd1de1a09fd2d10a5d405afc0550c
EstenioGabriel <esteniogabrieldelima@gmail.com>
2026-05-22 21:12:21 -0300

    Script para criar o DB

---
+++ 01_create_database.sql
@@ -0,0 +1,4 @@
+-- 01_create_database.sql
+DROP DATABASE IF EXISTS clinica_veterinaria;
+CREATE DATABASE clinica_veterinaria DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_unicode_ci;
+USE clinica_veterinaria;
